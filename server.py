28eb5bedbfc1bbf16b2b66e79fd754a0968b0ef7
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-09-08 20:57:31 -0700

    Added /diss route and connected route requests to one another

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html><a href="/hello">Hello Website</a><br><html>Hi! This is the home page.</html>"""
 
 
 @app.route('/hello')
@@ -35,8 +35,37 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
-        </form>
+          <label for="compliment-select"> Pick a compliment:</label>
+
+          <select name="compliment" id="compliment-select">
+            <option value="awesome">Awesome</option>
+            <option value="terrific">Terrific</option>
+            <option value="fantastic">Fantastic</option>
+            <option value="neato">Neato</option>
+            <option value="fantabulous">Fantabulous</option>
+            <option value="wowza">Wowza</option>
+            <option value="oh-so-not-meh">Oh-So-Not-Meh'</option>
+
+            <option value="brilliant">Brilliant</option>
+            <option value="ducky">Ducky</option>
+            <option value="coolio">Coolio</option>
+            <option value="incredible">Incredible</option>
+            <option value="wonderful">Wonderful</option>
+            <option value="smashing">Smashing</option>
+            <option value="lovely">Lovely</option>
+            <input type="submit" value="Submit">
+          </form>
+          <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <label for="insult-select"> Pick an insult:</label>
+
+          <select name="insult" id="insult-select">
+            <option value="awful">Awful</option>
+            <option value="theworst">The Worst</option>
+            <option value="ugly">Ugly</option>
+            <input type="submit" value="Submit">
+          </form>
+
       </body>
     </html>
     """
@@ -48,7 +77,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return f"""
     <!doctype html>
@@ -62,6 +91,25 @@ def greet_person():
     </html>
     """
 
+@app.route('/diss')
+def insult_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    insult = request.args.get("insult")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insult}!
+      </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
